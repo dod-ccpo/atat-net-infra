8c241f1a04cfddd4be3138d5566cfcdf265d7856
Zach McClurg <zach.mcclurg-ctr@ccpo.mil>
2023-10-10 15:22:37 -0400

    adding test to pipeline

---
+++ test/atat-net-infrastacl.test.ts
@@ -0,0 +1,45 @@
+import * as cdk from "aws-cdk-lib";
+import { Template } from "aws-cdk-lib/assertions";
+import { AtatPipelineStack } from "../lib/atat-net-infra-stack";
+
+const TEST_BRANCH_NAME = "test";
+
+describe("Validate creation of the pipeline stack", () => {
+  let app: cdk.App;
+  let stack: AtatPipelineStack;
+  let template: Template;
+
+  beforeEach(() => {
+    app = new cdk.App();
+    stack = new AtatPipelineStack(app, "TestPipelineStack", {
+      environmentName: "At0000",
+      repository: "test/test",
+      vpcCidr: "10.0.0.0",
+      orgARN: "X-OREFDSE",
+      branch: TEST_BRANCH_NAME,
+      githubPatName: "DOES_NOT_EXIST",
+      notificationEmail: "test@example.com"
+    });
+    template = Template.fromStack(stack);
+  });
+
+  it("should contain at least one CodeBuild project", async () => {
+    template.hasResourceProperties("AWS::CodeBuild::Project", {});
+  });
+
+  it("should have exactly one CodePipeline project", async () => {
+    template.resourceCountIs("AWS::CodePipeline::Pipeline", 1);
+  });
+
+  it("should leverage webhooks for a single branch", async () => {
+    template.hasResourceProperties("AWS::CodePipeline::Webhook", {
+      Filters: [
+        {
+          JsonPath: "$.ref",
+          MatchEquals: "refs/heads/{Branch}",
+        },
+      ],
+      RegisterWithThirdParty: true,
+    });
+  });
+});

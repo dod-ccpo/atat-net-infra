33b189520e4303ba2b58b732e1910a733e490cc8
Zach McClurg <zach.mcclurg-ctr@ccpo.mil>
2023-10-16 17:16:29 -0400

    added test file for the pipeline stage file in lib

--- test/atat-net-infra-pipeline-stage.test.ts
+++ test/atat-net-infra-pipeline-stage.test.ts
@@ -1,12 +1,18 @@
 import * as cdk from "aws-cdk-lib";
 import * as AtatNetFirewall from "../lib/atat-net-infra-pipeline-stage";
 
+// describe("Validate creation of the pipeline stack", () => {
+//     let app: cdk.App;
+//     let stack: AtatNetFirewall.NetInfraPipelineStage;
+
 test("The stack creates successfully with a VPC Firewall defined", () => {
   // GIVEN
   const app = new cdk.App();
   // WHEN
-  const network = new AtatNetFirewall.NetInfraPipelineStage(app, "TestNetStack", {
+  const network = new AtatNetFirewall.NetInfraPipelineStage(app, "TestFirewallStack", {
     orgARN: "y-675jgh",
     environmentName: "TEST",
   });
 });
+// }
+// )

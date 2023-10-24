e2fbe5b71aba7497389ec78c259a6fc8562fd786
Zach McClurg <zach.mcclurg-ctr@ccpo.mil>
2023-10-23 21:42:09 -0400

    working on updating read.me document

--- lib/atat-net-infra-firewall-vpc.ts
+++ lib/atat-net-infra-firewall-vpc.ts
@@ -129,31 +129,7 @@ export class FirewallVpcStack extends cdk.Stack {
 
         // 
         // TGW VPC Attachment
-        // 
-        const eventbus = new events.EventBus(this, 'TGW-Bus-Event', {
-          eventBusName: 'ATAT-Event-Bus'
-        });
-        eventbus.addToResourcePolicy(new iam.PolicyStatement({
-          sid: 'TransitBusEventPolicy',
-          effect: iam.Effect.ALLOW,
-          actions: ['events:PutEvents'],
-          principals: [new iam.StarPrincipal()],
-          resources: [eventbus.eventBusArn],
-          conditions: {
-            'StringEquals': {
-              'aws:PrincipalOrgID': props.orgARN,
-        },
-      },
-      }));
-        
-        // eventbus.archive('MyArchive', {
-        //   archiveName: 'MyCustomEventBusArchive',
-        //   description: 'MyCustomerEventBus Archive',
-        //   eventPattern: {
-        //     account: [cdk.Stack.of(this).account],
-        //   },
-        //   retention: cdk.Duration.days(365),
-        // });
+        //
         
         const tgwAttachment = new ec2.CfnTransitGatewayAttachment(this, 'tgwAttachment', {
             transitGatewayId: props.tgwId,

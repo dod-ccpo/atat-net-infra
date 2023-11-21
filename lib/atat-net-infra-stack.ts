b5c4ceb36a9dc8fefeb11715d73800f60d216890
Zach McClurg <zach.mcclurg-ctr@ccpo.mil>
2023-11-21 09:48:03 -0500

    changed pipeline repo

--- lib/atat-net-infra-stack.ts
+++ lib/atat-net-infra-stack.ts
@@ -1,12 +1,15 @@
 import { Duration, Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as cdk from "aws-cdk-lib";
+import * as codecommit from "aws-cdk-lib/aws-codecommit";
+import * as iam from "aws-cdk-lib/aws-iam";
 import * as pipelines from "aws-cdk-lib/pipelines";
 import { GovCloudCompatibilityAspect } from "./aspects/govcloud-compatibility";
 // import { NagSuppressions, NIST80053R4Checks } from "cdk-nag";
 import { AtatContextValue } from "./context-values";
 import { NetInfraPipelineStage } from "./atat-net-infra-pipeline-stage"
 import * as ssm from 'aws-cdk-lib/aws-ssm'
+import { NagSuppressions } from "cdk-nag";
 
 export interface AtatProps {
   environmentName: string;
@@ -37,13 +40,35 @@ export class AtatPipelineStack extends cdk.Stack {
       //AtatContextValue.NOTIFICATION_EMAIL.toCliArgument(props.notificationEmail),
     ];
 
+    const repo = new codecommit.Repository(this, "ATAT-Repository", {
+      repositoryName: "ATAT-CC-" + props.environmentName + "-Repo",
+    });
+
+    const user = new iam.User(this, "ATAT-CodeCommit-User", {});
+
+    const policy = new iam.Policy(this, "ATAT-Gitlab-UserPolicy", {
+      policyName: "ATAT-Gitlab-UserPolicy",
+      statements: [
+        new iam.PolicyStatement({
+          effect: iam.Effect.ALLOW,
+          actions: ["codecommit:GitPull", "codecommit:GitPush"],
+          resources: [repo.repositoryArn],
+        }),
+      ],
+    });
+
+    NagSuppressions.addResourceSuppressions(user, [
+      {
+        id: "NIST.800.53.R4-IAMUserGroupMembership",
+        reason: "The IAM user does not belong to any group(s)",
+      },
+    ]);
+
+    policy.attachToUser(user);
+
     const pipeline = new pipelines.CodePipeline(this, "Pipeline", {
       synth: new pipelines.ShellStep("Synth", {
-        input: pipelines.CodePipelineSource.gitHub(props.repository, props.branch, {
-          authentication: cdk.SecretValue.secretsManager(props.githubPatName, {
-            versionId: AtatContextValue.FORCE_GITHUB_TOKEN_VERSION.resolve(this),
-          }),
-        }),
+        input: pipelines.CodePipelineSource.codeCommit(repo, props.branch),
         commands: ["npm ci", "npm run build", "npm run -- cdk synth " + synthParams.join(" ")],
       }),
       dockerEnabledForSynth: true,
